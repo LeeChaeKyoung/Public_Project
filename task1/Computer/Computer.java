c32498203090ed874bd82d6fc30949f6c3a80756
LeeChaeKyoung <97029087+LeeChaeKyoung@users.noreply.github.com>
2022-01-03 23:15:32 +0900

    Add files via upload

---
+++ task1/Computer/Computer.java
@@ -0,0 +1,33 @@
+package task1;
+
+class mainframe{
+
+}
+
+class cpu extends mainframe{
+
+}
+
+class ssd extends cpu{
+
+}
+
+class grapicCard extends ssd{
+
+}
+
+class power extends grapicCard {
+
+}
+
+class cooler extends power {
+
+}
+
+class ram extends cooler{
+
+}
+
+public class Computer extends ram{
+    
+}
